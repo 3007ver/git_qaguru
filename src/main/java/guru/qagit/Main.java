43d32be47cb813b215263eda9ff6e6820cd2c36b
3007ver <veronika.shyshlo@gmail.com>
2023-04-03 23:10:45 +0300

    added new method

--- src/main/java/guru/qagit/Main.java
+++ src/main/java/guru/qagit/Main.java
@@ -1,7 +1,13 @@
 package guru.qagit;
 
 public class Main {
+    private static String msg = "Hello qa.guru";
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(getMessage());
+    }
+
+    private static String getMessage() {
+        return msg;
     }
 }
